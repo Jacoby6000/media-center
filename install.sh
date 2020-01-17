48ab3f7dcf36cb5be6d97847d5e58aa419486bc7
Jacob Barber <jacob.barber@disneystreaming.com>
2020-01-17 15:30:16 -0500

    set up nginx

--- install.sh
+++ install.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Pulling images..."
+echo ""
+sleep 1
+
+IMAGES=tautulli,plex,sonarr,radarr,jackett
+for image in ${IMAGES//,/ } ; do
+  docker pull linuxserver/$image
+done
+
+echo ""
+echo ""
+echo "Done pulling images. Linking systemd services and nginx confs."
+echo ""
+sleep 1
+
 SERVICES=tautulli,plex,sonarr,radarr,jackett,sonarr-anime
 for service in ${SERVICES//,/ } ; do
-  cp $service/$service.service /etc/systemd/system/
+  echo ""
+  echo "Linking $service systemd service."
+
+  rm -f /etc/systemd/system/$service.service
+  ln -s $DIR/$service/$service.service /etc/systemd/system/
+
+  echo "Configuring nginx for $service."
+  rm -f /etc/nginx/sites-available/$service.conf
+  ln -s $DIR/$service/$service.conf /etc/nginx/sites-available
+
+  rm -f /etc/nginx/sites-enabled/$service.conf
+  ln -s /etc/nginx/sites-available/$service.conf /etc/nginx/sites-enabled/$service.conf
+
 done
+
+echo ""
+echo "Reloading nginx configuration"
+systemctl reload nginx
+
+echo ""
+echo "Done."
 
